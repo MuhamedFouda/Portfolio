f18f361ae9152960a0b98074e1e67442c414833d
MuhamedFouda <memofouda59@gmail.com>
2024-12-14 15:37:28 +0200

    first changes

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -49,7 +49,7 @@ export default function Services() {
                 </div>
                 <div className="title">
                     <h1> <motion.b whileHover={{ color: 'orange' }}>For Your</motion.b> Business </h1>
-                    <button> WHAT WE DO ?</button>
+                    <button> WHAT I DO </button>
                 </div>
             </motion.div>
             <motion.div className="listContainer" variants={variants}>
